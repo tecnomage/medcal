831d9aca07e33e81acf9043f287b9c5461df0b5c
portal 3 <portal 3@portal3-PC>
2016-11-07 19:05:03 -0200

    adicionando boot.java

--- src/main/java/br/com/medclin/boot/Boot.java
+++ src/main/java/br/com/medclin/boot/Boot.java
@@ -1,27 +1,25 @@
 package br.com.medclin.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@Controller
+@EnableAutoConfiguration
 public class Boot
 {
 
-   public static void main(String[] args)
-   {
+   public static void main(String[] args) {
 	   ApplicationContext ctx = SpringApplication.run(Boot.class, args);
 	   System.out.println(ctx.getDisplayName());
 	  
 	   SpringApplication.run(Boot.class, args);
-   }
-
+}
    @RequestMapping("/")
    @ResponseBody
    public String home()
@@ -31,7 +29,3 @@ public class Boot
 
 }
 
-//@ComponentScan(basePackages = {"br.com.medclin.models" , "br.com.medclin.controllers",
-//"br.com.medclin.daos","br.com.medclin"})
-//@ComponentScan(basePackages = {"br.com.medclin.models" , "br.com.medclin.controllers",
-//		"br.com.medclin.daos","br.com.medclin.boot"})
