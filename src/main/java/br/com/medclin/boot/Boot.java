d20e08024579437cc9bd37af697672ce7754e4a9
portal 3 <portal 3@portal3-PC>
2016-11-07 21:09:44 -0200

    está dando erro na config da jpa

--- src/main/java/br/com/medclin/boot/Boot.java
+++ src/main/java/br/com/medclin/boot/Boot.java
@@ -4,27 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@Controller
 public class Boot
 {
 
    public static void main(String[] args) {
 	   ApplicationContext ctx = SpringApplication.run(Boot.class, args);
-	   System.out.println(ctx.getDisplayName());
+	   System.out.println("ola meus amigos" + ctx.getDisplayName());
 	  
-	   SpringApplication.run(Boot.class, args);
+	   //SpringApplication.run(Boot.class, args);
 }
    @RequestMapping("/")
    @ResponseBody
    public String home()
    {
-      return "home";
+      return "index";
    }
 
 }
