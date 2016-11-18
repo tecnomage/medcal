494ded1ffdea45e19c0f934147dccfd99355f34a
tecnomage <tecnocratay@gmail.com>
2016-11-18 17:04:56 -0200

    BuscaTOdos OK

--- src/test/br/com/medclin/boot/daosTest/PacienteDaoTest.java
+++ src/test/br/com/medclin/boot/daosTest/PacienteDaoTest.java
@@ -1,10 +1,15 @@
 package br.com.medclin.boot.daosTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Calendar;
+import java.util.List;
+
+import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,11 +20,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import br.com.medclin.boot.builders.CriadorDeMedico;
-import br.com.medclin.boot.builders.CriadorDePaciente;
-import br.com.medclin.boot.builders.CriadorDePacientes;
 import br.com.medclin.boot.daos.PacienteDao;
-import br.com.medclin.boot.models.Medico;
 import br.com.medclin.boot.models.Paciente;
 
 @RunWith(SpringRunner.class)
@@ -31,7 +32,7 @@ public class PacienteDaoTest {
 
 	@Autowired
 	private PacienteDao pctDao;
-	
+
 	@Autowired
 	private PacienteDao repository;
 
@@ -40,21 +41,69 @@ public class PacienteDaoTest {
 
 	@Before
 	public void prepareMocks() {
-		 pct = mock(Paciente.class);
-		 pctDao = mock(PacienteDao.class);
-			}
+		pct = mock(Paciente.class);
+		pctDao = mock(PacienteDao.class);
+		
+		this.entityManager.persist(new Paciente("vinicius", Calendar.getInstance(), "rua 10", "1"));
+		this.entityManager.persist(new Paciente("marcio", Calendar.getInstance(), "rua 10", "2"));
+		this.entityManager.persist(new Paciente("ius", Calendar.getInstance(), "rua 10", "3"));
+		this.entityManager.persist(new Paciente("mariozinho", Calendar.getInstance(), "rua 10", "123"));
+		this.entityManager.persist(new Paciente("marcela", Calendar.getInstance(), "rua 10", "4"));
+		
+		
+	}
 
 	@Test
 	public void buscaPorCpfMockado() {
-		
+
 		when(pct.getCpf()).thenReturn("123");
 
 		when(pctDao.findByCpf("123")).thenReturn(pct);
-		
+
 		Paciente pctRepoMocado = pctDao.findByCpf("123");
-		
+
 		assertEquals("123", pct.getCpf());
+
+	}
+
+	@Test
+	public void BuscaPorNome() {
+		
+		Paciente pct = this.repository.findByNome("vinicius");
+		
+		assertEquals("vinicius",pct.getNome());
+	}
+	
+	@Test
+	public void BuscaPorCpf() {
+		
+		
+		
+		Paciente paciente = this.repository.findByCpf("123");
+		
+		// Paciente pctAux = new Paciente("123");
+		
+		assertThat(paciente.getCpf()).isEqualTo("123");
 		
 	}
+	
+	@Test
+	public void BuscaTodos() {
+		
+
+		List<Paciente> pcts = (List<Paciente>) repository.findAll(); 
+		
+		List<Paciente> pcts2 = (List<Paciente>) repository.findAll();
+		
+		for (Paciente paciente : pcts2) {
+			System.out.println( paciente.getNome());
+		}
+		
+		assertThat(pcts,
+				IsIterableContainingInOrder.contains(pcts2.toArray()));
+		
+	}
+	
+	
 
 }
