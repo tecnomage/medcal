e599fb2d34117e6ca1db3d849f7a6d4e027bfb61
kali <kali@kali-System-Product-Name>
2016-12-01 01:09:01 -0200

    cpf is comming null

--- src/main/java/br/com/medclin/boot/controllers/PacienteController.java
+++ src/main/java/br/com/medclin/boot/controllers/PacienteController.java
@@ -1,15 +1,23 @@
 package br.com.medclin.boot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.medclin.boot.daos.PacienteDao;
 import br.com.medclin.boot.models.Paciente;
 
 @Controller
 @RequestMapping("medclin/paciente")
 public class PacienteController {
 
+	
+	@Autowired
+	PacienteDao pacientedao;
+	
 	
 	@RequestMapping("/cadastro")
 	public ModelAndView Cadastro() {
@@ -21,6 +29,19 @@ public class PacienteController {
 	}
 	
 	
+	//TODO fazer o tymeleaf date formate antes de salvar.
+	@RequestMapping(value="/salvar" , method = RequestMethod.POST)
+	public ModelAndView salvar(@ModelAttribute Paciente paciente){
+		
+		System.out.println(paciente.toString()); 
+		pacientedao.save(paciente);
+		ModelAndView modelAndView = new ModelAndView("/listar");
+		
+		
+		return modelAndView;
+		
+		
+	}
 	
 	
 		
